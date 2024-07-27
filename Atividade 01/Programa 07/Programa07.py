b7b1fa548d86ee7277b46665b93ec348b11d2689
Mauro Sousa <maurojosencfds@gmail.com>
2024-07-27 09:51:40 -0300

    ajeitando o programa07

--- Atividade 01/Programa 07/Programa07.py
+++ Atividade 01/Programa 07/Programa07.py
@@ -7,7 +7,7 @@ import cv2
 imagem_cinza = cv2.imread('imagem_webcam.jpg', cv2.IMREAD_GRAYSCALE)
 
 # Aplicação do regular thresholding
-thresholding_regular = cv2.threshold(imagem_cinza, 127, 255, cv2.THRESH_BINARY)
+_, thresholding_regular = cv2.threshold(imagem_cinza, 127, 255, cv2.THRESH_BINARY)
 cv2.imshow('Regular thresholding', thresholding_regular)
 
 # Aplicação do adaptive thresholding
