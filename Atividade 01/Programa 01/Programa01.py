39c371de73dce8ccd0dd0fc3dc355676f87821af
Mauro Sousa <maurojosencfds@gmail.com>
2024-07-27 19:53:39 -0300

    Modificando o programa 01

--- Atividade 01/Programa 01/Programa01.py
+++ Atividade 01/Programa 01/Programa01.py
@@ -1,15 +1,9 @@
-# Carregar e Exibir Imagem do disco
-
 import cv2
 
-# Caminho para a imagem
-caminho_imagem = 'caminho/para/imagem.jpg/png'
+# Carregar a imagem
+imagem = cv2.imread('caminho/para/imagem.jpg')
 
-# Carregar uma imagem do disco
-imagem = cv2.imread(caminho_imagem)
-if imagem is not None:
-    cv2.imshow('Imagem Carregada', imagem)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-else:
-    print("Erro ao carregar a imagem.")
+# Exibir a imagem
+cv2.imshow('Imagem', imagem)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
